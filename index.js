2d5e55c3cf8e34636fb45dda9e726ea95568c9f4
north <luanwillianll@hotmail.com>
2023-03-04 14:59:04 -0300

    add commands

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-
-const { Client, Events, GetwayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [GetwayIntentBits.guilds]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[ERRO!] o comando em ${filePath} está faltando uma propriedade derequerimento "data" ou "execute"...`);
+    }
+}
+
 client.once(Events.ClientReady, c=> {
-    console.log('Pronto! Loguin por: ${c.user.tag}')
+    console.log(`Pronto! Login por: ${c.user.tag}`)
+});
+
+client.on(Events.InteractionCreate, interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	console.log(interaction);
 });
 
 client.login(token);
