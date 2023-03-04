ad3d9315f2acd0e7a6250acd5712f734e172e85f
north <luanwillianll@hotmail.com>
2023-03-04 13:14:58 -0300

    atualização 0.0.1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-var teste = "hello world!"
+
+const { Client, Events, GetwayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GetwayIntentBits.guilds]});
+client.once(Events.ClientReady, c=> {
+    console.log('Pronto! Loguin por: ${c.user.tag}')
+});
+
+client.login(token);
